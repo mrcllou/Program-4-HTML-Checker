695b2edc5eb016774f71f9dd218ab959f4789b53
Marcelline <marcelle.andrianjaka@gmail.com>
2026-05-22 21:09:48 -0700

    Add return Tags + Stack in the fixHTML

--- HTMLManager.java
+++ HTMLManager.java
@@ -4,22 +4,28 @@ public class HTMLManager {
   private Queue<HTMLTag> tags;
   
    public HTMLManager(Queue<HTMLTag> html) {
+        tags = new LinkedList<>();
         if (html == null) {
            throw new IllegalArgumentException("null");
         } 
-        
-        tags = new LinkedList<Integers>();
         
         while(!html.isEmpty()){
         }
     }
     
     public Queue<HTMLTag> getTags(){
+      return tags; 
+    }
+    
+    public void fixHTML(){
+      Stack<HTMLTag> openings = new Stack<HTMLtag>();
+      int size = this.tags.size();
+      for(int i = 0; i < size; i++){
+      }
     }
     
     public String toString(){
     }
     
-    public void fixHTML(){
-    }
+    
 }
